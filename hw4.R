fe74606db60b6f6d139b8cefa2c1d91569ee5899
WenyaWeiss <61776544+WenyaWeiss@users.noreply.github.com>
2021-10-07 10:04:54 +0300

    Add files via upload

--- hw4.R
+++ hw4.R
@@ -8,14 +8,11 @@ library(extrafont)
 library(ggtext)
 library(wesanderson)
 
+windowsFonts("Slayer"=windowsFont("Slayer"))
 import_avatar()
-loadfonts()
 
 
 avatar <- read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-11/avatar.csv")
-
-avatar
-
 
 sorted_avatar <- avatar %>%
   mutate(chat_len = str_count(character_words, "\\W+")) %>%
@@ -31,6 +28,7 @@ temp <- sorted_avatar %>%
 
 elected <- temp$character[1:5]
 
+
 sorted_avatar <- sorted_avatar %>%
   filter(character %in% elected) %>%
   group_by(chapter_num, character, book) %>%
@@ -40,32 +38,31 @@ sorted_avatar
 
 chat <- ggplot(sorted_avatar, aes(x = sorted_avatar$character, y = sorted_avatar$alchat, fill = character)) +
   geom_boxplot() +
-  scale_fill_manual(values=wes_palette(n=5, name="Darjeeling1" )) +
-  geom_point(size=3, alpha=0.5) +
-#  scale_fill_manual(values=wes_palette(n=5, name="Darjeeling1"))
+  scale_fill_manual(values=wes_palette(n=5, name="Cavalcanti1" )) +
+  geom_point(size=2, alpha=0.5) +
   
   facet_grid(cols = vars(book)) +
-#  theme_avatar(text.font = "Slayer",
-#               title.font = "Slayer") +
+  theme_avatar(text.font = "Slayer",
+               title.font = "Slayer") +
   
-  theme(plot.title = element_markdown(size = 20,
-                                      hjust = 0.06),
-        plot.subtitle = element_markdown(hjust = 0.1),
+  theme(plot.title = element_markdown(size = 15),
+        plot.subtitle = element_markdown(size = 10),
         plot.caption = element_text(margin = margin(t = 10)),
-        panel.spacing.y = unit(1, "lines"),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        strip.text = element_blank(),
+        panel.background = element_rect(colour = "black",
+                                        size = 0.5),
         legend.position = "none",
         axis.text = element_text(size = 10),
-        axis.title.y = element_text(size = 11,
+        axis.title.y = element_text(size = 10,
                                     margin = margin(r = 10)),
-        #axis.text.y = element_blank(),
-        axis.title.x = element_text(size = 11,
-                                    margin = margin(t = 10))) +
+        
+        axis.text.x = element_text(size = 10, 
+                                   angle = 45, 
+                                   margin = margin(t = 14)),
+        axis.title.x =  element_blank()) +
+  
   labs(x = "Characters", y = "Numbers of words spoken per episode", 
        title = " <br>Who is the chattiest character in<br><b>  Avatar: The Last Airbender</b>",
-       subtitle = "Five mist talkative characters have more than a hundred words every episode")
+       subtitle = "Five most talkative characters <br> have more than a hundred words every episode")
 
 
 chat
